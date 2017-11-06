62d70753fd583822673c36c9925a309912d75f77
王旭政 <wangxuzheng@aliyun.com>
2017-11-06 18:39:19 +0800

    '项目初始化'

---
+++ springcloud-oauth2/src/main/java/com/pachiraframework/springcloud/oauth2/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,32 @@
+package com.pachiraframework.springcloud.oauth2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+import com.pachiraframework.springcloud.oauth2.service.MyUserDetailsService;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@Override
+	public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+	}
+	@Override
+	public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		// @formatter:off
+		endpoints.authenticationManager(authenticationManager)
+				.userDetailsService(userDetailsService);
+		// @formatter:on
+	}
+}
---
+++ springcloud-oauth2/src/main/resources/templates/authorize.ftl
@@ -0,0 +1,29 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h2>Please Confirm</h2>
+
+		<p>
+			Do you authorize "${authorizationRequest.clientId}" at "${authorizationRequest.redirectUri}" to access your protected resources
+			with scope ${authorizationRequest.scope?join(", ")}.
+		</p>
+		<form id="confirmationForm" name="confirmationForm"
+			action="../oauth/authorize" method="post">
+			<input name="user_oauth_approval" value="true" type="hidden" />
+			<input type="hidden" id="csrf_token" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+			<button class="btn btn-primary" type="submit">Approve</button>
+		</form>
+		<form id="denyForm" name="confirmationForm"
+			action="../oauth/authorize" method="post">
+			<input name="user_oauth_approval" value="false" type="hidden" />
+			<input type="hidden" id="csrf_token" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+			<button class="btn btn-primary" type="submit">Deny</button>
+		</form>
+	</div>
+<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
